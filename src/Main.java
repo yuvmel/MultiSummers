536d138f491827400f6c2edcce66417b34bc30ea
yuval.melamed <yuval.melamed@7450-171286.imperva.local>
2017-06-17 22:41:48 +0300

    Summing, no wait yet...

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,48 @@
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
-    public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new ArrayList<>();
+    private static final int MAX_RAND = 100;
 
+    private static int getPositiveInt(String message) {
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        while (i == 0) {
+            try {
+                System.out.println(message);
+                i = scanner.nextInt();
+            } catch (Exception e) {
+                scanner.nextLine();
+            }
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int n = getPositiveInt("Enter array size, n>0:");
+        int m = getPositiveInt("Enter thread count, m>0:");
+
+        ArrayList<Integer> arrayList = new ArrayList<>(n);
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            arrayList.add(random.nextInt(MAX_RAND) + 1);
+        }
+
+        SynchronizedCollection synchronizedCollection
+                = new SynchronizedCollection(arrayList);
+        System.out.println(synchronizedCollection);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(m);
+        for (int i = 0; i < m; i++) {
+            executorService.execute(new SummingThread(synchronizedCollection));
+        }
+        executorService.shutdown();
+
+        System.out.println(synchronizedCollection);
     }
 }
